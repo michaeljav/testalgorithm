53104edfb0d5978a62d726484e2fd638e736ced3
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-06 14:24:03 -0400

    validate string

--- Program.cs
+++ Program.cs
@@ -24,14 +24,28 @@ namespace testalgorithm
             return s.All(char.IsLower);
         }
 
+        static Boolean isPasswordComplex(string s)
+        {
+            return s.Any(char.IsUpper)
+                && s.Any(char.IsLower)
+                && s.Any(char.IsDigit);
+
+
+        }
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine(IsUppercase("HE"));
-            Console.WriteLine(IsUppercase("he"));
+            //Console.WriteLine(IsUppercase("HE"));
+            //Console.WriteLine(IsUppercase("he"));
 
-            Console.WriteLine(IsLowercase("HE"));
-            Console.WriteLine(IsLowercase("he"));
+            //Console.WriteLine(IsLowercase("HE"));
+            //Console.WriteLine(IsLowercase("he"));
+            
+            
+            Console.WriteLine(isPasswordComplex("Hell0"));
+            Console.WriteLine(isPasswordComplex("Hello"));
+            
 
         }
 
