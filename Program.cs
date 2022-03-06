cbe426815edcbde75e1b384705851574d548c4df
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-06 14:17:57 -0400

    Algorithm to uppercase and lowercase

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace testalgorithm
 {
@@ -13,63 +14,25 @@ namespace testalgorithm
 //}
 //stopWatch.Stop();
 //textbox1.Text = stopWatch.ElapsedMilliseconds.ToString();
-        static int findMaximum(int a, int b, int c) {
-            stopWatch.Start();
-            if (a > b)
-            {
-                if (a > c  || a == c)
-                {
-                    return a;
-                }
-            }
-            if (b > c)
-            {
-                if (b > a || a == b)
-                {
-                    return b;
-                }
-            }
-
-            stopWatch.Stop();
-            Console.WriteLine($"MilliSecond:  {stopWatch.ElapsedMilliseconds.ToString()}");
-            //Console.WriteLine($"MilliSecond:  {stopWatch.ElapsedTicks.ToString()}");
-            return c;
-        }
-
-        static int findMaximum2(int a, int b, int c)
+        static Boolean IsUppercase(string s)
         {
-            stopWatch.Start();
-            int max = a;
-
-            if(b > max )
-            {
-                max = b;
-            }
-
-            if(c > max)
-            {
-                max = c;
-            }
-
-            stopWatch.Stop();
-            Console.WriteLine($"MilliSecond: {stopWatch.ElapsedMilliseconds.ToString()}");
-            //Console.WriteLine($"MilliSecond:  {stopWatch.ElapsedTicks.ToString()}");
-            return max;
+            return s.All(char.IsUpper);
         }
+
+        static Boolean IsLowercase(string s)
+        {
+            return s.All(char.IsLower);
+        }
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine(findMaximum(1, 2, 3));
-            Console.WriteLine(findMaximum(8, 8, 1));
-            Console.WriteLine(findMaximum(3, 2, 3));
-            Console.WriteLine(findMaximum(1, 1, 9));
-            Console.WriteLine(findMaximum(1, 9, 9));
+            Console.WriteLine(IsUppercase("HE"));
+            Console.WriteLine(IsUppercase("he"));
 
-            Console.WriteLine(findMaximum2(1, 2, 3));
-            Console.WriteLine(findMaximum2(8, 8, 1));
-            Console.WriteLine(findMaximum2(3, 2, 3));
-            Console.WriteLine(findMaximum2(1, 1, 9));
-            Console.WriteLine(findMaximum2(1, 9, 9));
+            Console.WriteLine(IsLowercase("HE"));
+            Console.WriteLine(IsLowercase("he"));
+
         }
 
       
