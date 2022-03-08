d6dbba15eb73d5ee900fc37ed0f663f5c248ff6a
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-08 18:56:59 -0400

    Linear serach Array

--- Program.cs
+++ Program.cs
@@ -8,100 +8,29 @@ namespace testalgorithm
     {
 
       
-        static  Stopwatch stopWatch = new Stopwatch();
-//        stopWatch.Start();
-//for (.......)
-//{
-//}
-//stopWatch.Stop();
-//textbox1.Text = stopWatch.ElapsedMilliseconds.ToString();
-      
-        static string ReverseEachWord(String input)
+          static Boolean LinearSearch(int[] input, int n)
         {
-            if (string.IsNullOrEmpty(input))
+            foreach (int current in input)
             {
-                return input;
-            }
-
-            StringBuilder result = new StringBuilder();
-            String[] arr = input.Split(" ");
-            for (int i = 0; i < arr.Length; i++)
-            {
-                result.Append(Reverse2(arr[i]));
-                if (i != arr.Length - 1)
+                if (n == current)
                 {
-                    result.Append(" ");
+                    return true;
                 }
-
             }
-            return result.ToString();
+
+            return false;
         }
-
-        static String  Reverse(string input)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                return input;
-            }
-
-            StringBuilder reversed = new StringBuilder(input.Length);
-
-            for (int i = input.Length - 1 ; i >= 0; i--)
-            {
-                reversed.Append(input[i]);
-            }
-            return reversed.ToString();
-
-        }
-
-        static String  Reverse2(String input)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                return input;
-            }
-
-            char[] arr = input.ToCharArray();
-             Array.Reverse(arr);
-            return new string(arr);
-        }
-
 
         static void Main(string[] args)
         {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+           Console.WriteLine(LinearSearch(arr, 4));
+           Console.WriteLine(LinearSearch(arr, 8));
 
-
-            //Console.WriteLine(Reverse("Hello World"));
-            //Console.WriteLine(Reverse(""));
-            //Console.WriteLine(Reverse("hello"));
-            //Console.WriteLine(Reverse("tacos"));
-            //Console.WriteLine(Reverse("Hi!"));
-            //Console.WriteLine(Reverse(null));
-
-            //Console.WriteLine(Reverse2("Hello World"));
-            //Console.WriteLine(Reverse2(""));
-            //Console.WriteLine(Reverse2("hello"));
-            //Console.WriteLine(Reverse2("tacos"));
-            //Console.WriteLine(Reverse2("Hi!"));
-            //Console.WriteLine(Reverse2(null));
-
-
-            //Console.WriteLine(Reverse2("Taylor is a great worker"));
-            //Console.WriteLine(Reverse2(null));
-            //Console.WriteLine(Reverse2("racer racecar madam"));
-            //Console.WriteLine(Reverse2("what can I do today"));
-            //Console.WriteLine(Reverse2(" "));
-
-
-            Console.WriteLine(("++++++++++++++++++++++++ReverseEachWord"));
-            Console.WriteLine(ReverseEachWord("Taylor is a great worker"));
-            Console.WriteLine(ReverseEachWord(null));
-            Console.WriteLine(ReverseEachWord("racer racecar madam"));
-            Console.WriteLine(ReverseEachWord("what can I do today"));
-            Console.WriteLine(ReverseEachWord(" "));
-
-
-
+            int item = Array.Find(arr, element => element == 3);
+            int[] items = Array.FindAll(arr, element => element >= 5);
+            Array.ForEach(items , Console.WriteLine);
+           
 
 
         }
