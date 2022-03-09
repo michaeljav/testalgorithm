8cee0ffd17b192f32f8ac77ca2f43b5f5e79107e
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-08 20:29:35 -0400

    Binary Search Arrays.
    *ordered
    *search in the middle

--- Program.cs
+++ Program.cs
@@ -6,31 +6,56 @@ namespace testalgorithm
 {
     class Program
     {
-
+        /**
+         * 
+         * 
+         * BINARY SEARCH : IF YOU HAVE A ORGANISED ARRAY  YOU USE THIS.         * 
+         * YOU BEGIN SEARCHING FROM THE MIDDLE.
+         * EJE :with this method we're  assuming that the array is sorted
+         * and  it is in ascending order.
+         * bINARY SEARCH is a divide and conquer algorithm
+         * 
+         * 
+         * 
+         * Note: Linear search: no assumptions, O(n)times
+         * Binary search: sorted assumption, O(log(n)) time
+         */
       
-          static Boolean LinearSearch(int[] input, int n)
+          static Boolean BinarySearch(int[] inputArray, int item)
         {
-            foreach (int current in input)
+            int min = 0;
+            int max = inputArray.Length - 1;
+
+            while (min <= max)
             {
-                if (n == current)
+                int mid = (min + max) / 2;
+                if (item == inputArray[mid])
                 {
                     return true;
+                }
+                else if(item < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
                 }
             }
 
             return false;
+
+
         }
 
         static void Main(string[] args)
         {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
-           Console.WriteLine(LinearSearch(arr, 4));
-           Console.WriteLine(LinearSearch(arr, 8));
+           Console.WriteLine(BinarySearch(arr, 5));
 
-            int item = Array.Find(arr, element => element == 3);
-            int[] items = Array.FindAll(arr, element => element >= 5);
-            Array.ForEach(items , Console.WriteLine);
-           
+            // C SHARP DOES HAVE A BUILT-IN FUNCTION FOR BINARY SEARCH
+           int result= Array.BinarySearch(arr, 1);
+            Console.WriteLine(result);
 
 
         }
