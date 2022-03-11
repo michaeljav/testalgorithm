d3e19939b92370bc7c37e41bc7df139c716e0790
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-11 12:56:17 -0400

    Rotate an Array by on position My solution

--- Program.cs
+++ Program.cs
@@ -7,58 +7,65 @@ namespace testalgorithm
 {
     class Program
     {
-        /**
-         * 
-         * 
-         * BINARY SEARCH : IF YOU HAVE A ORGANISED ARRAY  YOU USE THIS.         * 
-         * YOU BEGIN SEARCHING FROM THE MIDDLE.
-         * EJE :with this method we're  assuming that the array is sorted
-         * and  it is in ascending order.
-         * bINARY SEARCH is a divide and conquer algorithm
-         * 
-         * 
-         * 
-         * Note: Linear search: no assumptions, O(n)times
-         * Binary search: sorted assumption, O(log(n)) time
-         */
-      
-          static int[] Reverse(int[] input)
+     
+        static void RotateOnPositionInPlace(int[] item)
         {
-
-            int[] reversed = new int[input.Length];
-
-            for (int i = 0; i < reversed.Length; i++)
+            int temp = item[0];
+            
+            for (int i = 0; i < item.Length; i++)
             {
-                reversed[i] = input[input.Length - i - 1];
+                //last item
+                if (i < item.Length -1)
+                {
+                    item[i] = item[i + 1];
+                }
+                else
+                {
+                    item[i] = temp;
+                }
+                
             }
-            return reversed;
         }
 
-        static void ReverseInPlace(int[] input)
+        static void RotateOnPositionInPlaceToRightPlace(int[] item)
         {
-            //loop untill mid because inside of for I swap the values.
-            for (int i = 0; i < input.Length / 2; i++)
+
+            int temp = item[item.Length - 1];
+
+            for (int i = 0; i < item.Length -1; i++)
+            //for (int i = 0; i < item.Length; i++)
             {
-                //Swap index(i) with index(input.Length-i-1)
-                int temp = input[i];
-                input[i] = input[input.Length - i - 1];
-                input[input.Length - i - 1] = temp;
+
+                
+
+
+                if (i == 0)
+                {
+                    int curr = item[i];
+                    item[i] = temp;
+                    item[item.Length - 1] = curr;
+                }
+                else
+                {
+                    int current = item[i];
+                    item[i] = item[item.Length - 1];
+                    item[item.Length - 1] = current;
+
+                }
+
+
             }
         }
 
         static void Main(string[] args)
         {
-            //6,5,4,3,2,1=
+            
             int[] arr = { 1,2,3,4,5,6 };
 
-            //OPTION 1 REVERSE
-            int[] eventArr = Reverse(arr);
-            Array.ForEach(eventArr, Console.WriteLine);
-
-            //OPTION 2 REVERSE WITH SWAP IN PLACE
-            ReverseInPlace(arr);
-            //THE REVERSE OF THE REVERSE IS THE ORIGINAL CONTENT
-            //ReverseInPlace(arr);
+           RotateOnPositionInPlace(arr);
+            Array.ForEach(arr, Console.WriteLine);
+            Console.WriteLine("TEST+++++");
+            RotateOnPositionInPlaceToRightPlace(arr);            
             Array.ForEach(arr, Console.WriteLine);
         }
 
