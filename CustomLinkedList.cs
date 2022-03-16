7fd76162cf032e9275f3d5e3496520bbce3a245d
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-15 20:16:30 -0400

    Linked list customed  First Explanation

---
+++ CustomLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testalgorithm
+{
+    class CustomLinkedList
+    {
+        Node head;
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d) { data = d; }
+        }
+
+        public void deleteBackHalf()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + "->");
+                current = current.next;
+            }
+        }
+       
+       
+        static void Main(string[] args)
+        {
+            CustomLinkedList linkedlist = new CustomLinkedList();
+            Node firstNode = new Node(3);
+            Node secondNode = new Node(4);
+            Node thirdNode = new Node(5);
+            Node fourthNode = new Node(6);
+
+            linkedlist.head = firstNode;
+            firstNode.next = secondNode;
+            secondNode.next = thirdNode;
+            thirdNode.next = fourthNode;
+
+            linkedlist.deleteBackHalf();
+            
+
+        }
+    }
+}
