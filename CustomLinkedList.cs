ea902e14fe92bddd529cfee848f02abb8435a921
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-15 20:46:21 -0400

    Linked list custom Third
    Traing to understaind because in this exercise is deleted the back half of the list.

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -20,6 +20,26 @@ namespace testalgorithm
 
         public void deleteBackHalf()
         {
+           if (head == null || head.next == null)
+            {
+                head = null;
+            }
+
+            Node slow = head;
+            Node fast = head;
+            Node prev = null;
+
+            while (fast != null && fast.next !=null)
+            {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            //this delete the middle node and everything after it
+            prev.next = null;
+        }
+        public void displayContents()
+        {
             Node current = head;
             while (current != null)
             {
@@ -28,7 +48,6 @@ namespace testalgorithm
             }
         }
        
-       
         static void Main(string[] args)
         {
             CustomLinkedList linkedlist = new CustomLinkedList();
@@ -36,13 +55,21 @@ namespace testalgorithm
             Node secondNode = new Node(4);
             Node thirdNode = new Node(5);
             Node fourthNode = new Node(6);
+            Node fifNode = new Node(7);
+            Node sixthNode = new Node(8);
 
             linkedlist.head = firstNode;
             firstNode.next = secondNode;
             secondNode.next = thirdNode;
             thirdNode.next = fourthNode;
 
+            fourthNode.next = fifNode;
+            fifNode.next = sixthNode;
+
+            linkedlist.displayContents();
             linkedlist.deleteBackHalf();
+            Console.WriteLine();
+            linkedlist.displayContents();
             
 
         }
