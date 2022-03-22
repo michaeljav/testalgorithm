8fb71514a1cebc37ca48e342e672d1da5f4af85c
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-22 17:03:35 -0400

    queue

--- Algorithms.cs
+++ Algorithms.cs
@@ -11,31 +11,38 @@ namespace testalgorithm
     class Algorithms
     {
         
+        static void printBinary(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            for (int i = 0; i < n; i++)
+            {
+                int current = queue.Dequeue();
+                Console.WriteLine(current);
+                queue.Enqueue(current * 10);
+                queue.Enqueue(current * 10 + 1);
+
+            }
+            Console.WriteLine();
+
+        }
         static void Main(string[] args)
         {
 
 
-            Queue<int> queue = new Queue<int>();
+            printBinary(5);
+            printBinary(-2);
+            printBinary(0);
+            printBinary(2);
+            printBinary(8);
 
-            queue.Enqueue(1);
-            queue.Enqueue(8);
-            queue.Enqueue(20);
-            queue.Enqueue(23);
 
-            int removedItem = queue.Dequeue();
-            Console.WriteLine(removedItem);
-            Console.WriteLine(queue.Dequeue());
-            Console.WriteLine(queue.Peek());
 
-            int current;
-            while (queue.TryDequeue(out current))
-            {
-                Console.WriteLine(current);
-
-            }
-
-            
-            
+            Console.Read();
 
 
 
