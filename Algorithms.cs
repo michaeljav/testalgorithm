7ac0123c1ec8db42571113bef4f30e2979bdbb19
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-25 17:31:46 -0400

    stack

--- Algorithms.cs
+++ Algorithms.cs
@@ -1,51 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace testalgorithm
 {
 
-    
+
     class Algorithms
     {
         
-        static void printBinary(int n)
-        {
-            if (n <= 0)
-            {
-                return;
-            }
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(1);
-            for (int i = 0; i < n; i++)
-            {
-                int current = queue.Dequeue();
-                Console.WriteLine(current);
-                queue.Enqueue(current * 10);
-                queue.Enqueue(current * 10 + 1);
-
-            }
-            Console.WriteLine();
-
-        }
+        
         static void Main(string[] args)
         {
 
+            Stack<String> stack = new Stack<string>();
 
-            printBinary(5);
-            printBinary(-2);
-            printBinary(0);
-            printBinary(2);
-            printBinary(8);
+            Console.WriteLine("Start Main " );
+            stack.Push("Main");
+            stack.Push("ResponseBuilder");
+            stack.Push("CallExternalService");
+            Console.WriteLine("End "+stack.Pop());
+            stack.Push("ParseExternalDate");
+            Console.WriteLine("End " + stack.Pop());
+            Console.WriteLine("End " + stack.Pop());
+            Console.WriteLine("End " + stack.Pop());
 
-
-
-            Console.Read();
-
-
-
+            //stack.Peek()
+            //stack.TryPeek()
+            string itemNext;
+            Console.WriteLine(stack.TryPeek(out itemNext));
 
         }
     }
