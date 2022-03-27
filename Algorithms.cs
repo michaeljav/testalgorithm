d687a9aaeb68b3bbfcf484182fbb54ea4450a37b
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-27 14:41:55 -0400

    Using Recursion to implement tree traversals

--- Algorithms.cs
+++ Algorithms.cs
@@ -15,47 +15,85 @@ namespace testalgorithm
             public int Data { get; set; }
         }
 
-        class BinarySearchTree
+       class BinaryTree
         {
-            public static Node Insert(Node root, int value)
-            {
-                if (root ==null)
+          public  static void preOrderTreversal(Node root)
+	            {
+                if (root == null)
                 {
-                    root = new Node();
-                    root.Data = value;
-                 
-
+                    return;
                 }
-                else
-                {
-                    //if value is less than the root
-                    if (value < root.Data)
-                    {
-                        //insert on left
-                        root.Left = Insert(root.Left, value);
-                    }
-                    else if(value > root.Data)
-                    {
-                        //insert on right
-                        root.Right = Insert(root.Right, value);
-                    }
-
-                }
-                return root;
+                Console.WriteLine(root.Data + " ");
+                preOrderTreversal(root.Left);
+                preOrderTreversal(root.Right);
             }
+
+            public static void inOrderTreversal(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+
+                inOrderTreversal(root.Left);
+                Console.WriteLine(root.Data + " ");
+                inOrderTreversal(root.Right);
+            }
+
+            public static void postOrderTreversal(Node root)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+
+                postOrderTreversal(root.Left);               
+                postOrderTreversal(root.Right);
+                Console.WriteLine(root.Data + " ");
+            }
+
+
         }
 
         static void Main(string[] args)
         {
+            //       4
+            //    1     3
+            //  8   9  6
             Node rootNode = new Node();
             rootNode.Data = 4;
-            BinarySearchTree.Insert(rootNode, 2);
-            BinarySearchTree.Insert(rootNode, 3);
-            BinarySearchTree.Insert(rootNode, 5);
-            BinarySearchTree.Insert(rootNode, 6);
-            BinarySearchTree.Insert(rootNode, 4);
-            
-        } 
+
+            Node nodeOne = new Node();
+            nodeOne.Data = 1;
+
+            Node nodeThree = new Node();
+            nodeThree.Data = 3;
+
+            Node nodeEight = new Node();
+            nodeEight.Data = 8;
+
+            Node nodeNine = new Node();
+            nodeNine.Data = 9;
+
+            Node nodeSix = new Node();
+            nodeSix.Data = 6;
+
+            rootNode.Left = nodeOne;
+            rootNode.Right = nodeThree;
+            nodeOne.Left = nodeEight;
+            nodeOne.Right = nodeNine;
+            nodeThree.Left = nodeSix;
+
+            BinaryTree.preOrderTreversal(rootNode);
+            Console.WriteLine();
+
+
+            BinaryTree.inOrderTreversal(rootNode);
+            Console.WriteLine();
+
+            BinaryTree.postOrderTreversal(rootNode);
+            Console.WriteLine();
+        }
 
     }
 }
