ff488752641cf76b36d5a48f2daa3dd876a60ebc
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-26 21:43:57 -0400

    stack the next greater number

--- Algorithms.cs
+++ Algorithms.cs
@@ -7,27 +7,63 @@ namespace testalgorithm
     class Algorithms
     {
         
-        
+        static void printNextGreaterElement(int[] arr)
+        {
+            if (arr.Length <= 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int next = arr[i];
+                if (stack.Count > 0)
+                {
+                    int popped = stack.Pop();
+
+                    while (popped <  next)
+                    {
+                        Console.WriteLine(popped + "-->" + next);
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        popped = stack.Pop();
+
+                    }
+
+                    if (popped > next)
+                    {
+                        stack.Push(popped);
+                    }
+                }
+                stack.Push(next);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop() + "-->" + -1);
+            }
+
+
+        }
         static void Main(string[] args)
         {
 
-            Stack<String> stack = new Stack<string>();
+            int[] arr = new int[] { 15, 8, 4, 10 };
+            int[] arr2 = new int[] { 2 };
+            int[] arr3 = new int[] { 2, 3 };
+            int[] arr4 = new int[] {};
 
-            Console.WriteLine("Start Main " );
-            stack.Push("Main");
-            stack.Push("ResponseBuilder");
-            stack.Push("CallExternalService");
-            Console.WriteLine("End "+stack.Pop());
-            stack.Push("ParseExternalDate");
-            Console.WriteLine("End " + stack.Pop());
-            Console.WriteLine("End " + stack.Pop());
-            Console.WriteLine("End " + stack.Pop());
-
-            //stack.Peek()
-            //stack.TryPeek()
-            string itemNext;
-            Console.WriteLine(stack.TryPeek(out itemNext));
-
+            printNextGreaterElement(arr);
+            printNextGreaterElement(arr2);
+            printNextGreaterElement(arr3);
+            printNextGreaterElement(arr4);
+ 
         }
     }
 }
