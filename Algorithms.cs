b3ae1cfc221a9d8f5467f627d7b2ef779cee942b
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-03-26 22:14:24 -0400

    stack Matching Paretheses

--- Algorithms.cs
+++ Algorithms.cs
@@ -7,63 +7,114 @@ namespace testalgorithm
     class Algorithms
     {
         
-        static void printNextGreaterElement(int[] arr)
+        static bool hasMatchingParentheses(string s)
         {
-            if (arr.Length <= 0)
+            if (s.Length <= 0)
             {
-                Console.WriteLine();
-                return;
+                return false;
             }
 
-            Stack<int> stack = new Stack<int>();
-            stack.Push(arr[0]);
+            Stack<char> stack = new Stack<char>();
 
-            for (int i = 1; i < arr.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
-                int next = arr[i];
-                if (stack.Count > 0)
+                char current = s[i];
+
+                if (current == '(')
                 {
-                    int popped = stack.Pop();
-
-                    while (popped <  next)
-                    {
-                        Console.WriteLine(popped + "-->" + next);
-                        if (stack.Count == 0)
-                        {
-                            break;
-                        }
-                        popped = stack.Pop();
-
-                    }
-
-                    if (popped > next)
-                    {
-                        stack.Push(popped);
-                    }
+                    stack.Push(current);
+                    continue;
                 }
-                stack.Push(next);
-            }
 
-            while (stack.Count > 0)
-            {
-                Console.WriteLine(stack.Pop() + "-->" + -1);
+                if (current == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                    stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
             }
+            return stack.Count == 0;
 
 
         }
+
+        static bool hasMatchingParentheses2(string s)
+        {
+            if (s.Length <= 0)
+            {
+                return false;
+            }
+
+            int machingSymbleTracker = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char current = s[i];
+
+                if (current == '(')
+                {
+                    machingSymbleTracker++;
+                    continue;
+                }
+
+                if (current == ')')
+                {
+                    if (machingSymbleTracker > 0)
+                    {
+                        machingSymbleTracker--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+            }
+            return machingSymbleTracker == 0;
+
+
+        }
+
         static void Main(string[] args)
         {
 
-            int[] arr = new int[] { 15, 8, 4, 10 };
-            int[] arr2 = new int[] { 2 };
-            int[] arr3 = new int[] { 2, 3 };
-            int[] arr4 = new int[] {};
 
-            printNextGreaterElement(arr);
-            printNextGreaterElement(arr2);
-            printNextGreaterElement(arr3);
-            printNextGreaterElement(arr4);
- 
+            //Console.WriteLine(hasMatchingParentheses(""));
+            //Console.WriteLine(hasMatchingParentheses("((hello()))"));
+            //Console.WriteLine(hasMatchingParentheses("()(hello())"));
+            //Console.WriteLine(hasMatchingParentheses("((hello))"));
+            //Console.WriteLine(hasMatchingParentheses("(hello)"));
+
+            //Console.WriteLine();
+
+            //Console.WriteLine(hasMatchingParentheses("(hello("));
+            //Console.WriteLine(hasMatchingParentheses(")hello)"));
+            //Console.WriteLine(hasMatchingParentheses(")hello("));
+            //Console.WriteLine(hasMatchingParentheses("hello(("));
+            //Console.WriteLine(hasMatchingParentheses("(hello"));
+            //Console.WriteLine(hasMatchingParentheses("((hello)"));
+
+
+            Console.WriteLine(hasMatchingParentheses2("((hello()))"));
+            Console.WriteLine(hasMatchingParentheses2("()(hello())"));
+            Console.WriteLine(hasMatchingParentheses2("((hello))"));
+            Console.WriteLine(hasMatchingParentheses2("(hello)"));
+
+            Console.WriteLine();
+
+            Console.WriteLine(hasMatchingParentheses2("(hello("));
+            Console.WriteLine(hasMatchingParentheses2(")hello)"));
+            Console.WriteLine(hasMatchingParentheses2(")hello("));
+            Console.WriteLine(hasMatchingParentheses2("hello(("));
+            Console.WriteLine(hasMatchingParentheses2("(hello"));
+            Console.WriteLine(hasMatchingParentheses2("((hello)"));
+
         }
     }
 }
